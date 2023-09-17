4f0266c5d23bf309a539a6f655b1825cbb915b23
MozzLein <farhanminta1234@gmail.com>
2023-09-17 15:55:25 +0800

    First Commit

---
+++ app/src/main/java/com/example/tugas2_md_13020200169_farhan_syarif/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tugas2_md_13020200169_farhan_syarif
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Tugas2_MD_13020200169_Farhan_Syarif"
+include(":app")
